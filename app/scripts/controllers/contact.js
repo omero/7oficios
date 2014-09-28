2cfaf935b4fb1a758e9ea7b0379b63369e68b5db
deita0x <josdeita@gmail.com>
2014-09-28 11:18:34 -0500

    agregado esqueleto rutas y controladores

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 7oficiosApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the 7oficiosApp
+ */
+angular.module('7oficiosApp')
+  .controller('ContactCtrl', function ($scope) {
+  });
---
+++ app/scripts/controllers/top-employee.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 7oficiosApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the 7oficiosApp
+ */
+angular.module('7oficiosApp')
+  .controller('TopEmployeeCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
