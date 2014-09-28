c103798be6006f67e68cb8e2179dce7ca9138b2f
Omar Aguirre <omersguchigu@gmail.com>
2014-09-28 14:02:17 -0500

    my chnges

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,22 +18,26 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/top-employee', {
+      .when('/employee', {
+        templateUrl: 'views/employee.html',
+        controller: 'employeeController'
+      })
+      .when('/employee/:nid', {
         templateUrl: 'views/top-employee.html',
-        controller: 'TopEmployeeCtrl'
+        controller: 'employeeDetailController'
       })
       .when('/catalog', {
         templateUrl: 'views/catalog.html',
         controller: 'CatalogCtrl'
       })
-      .when('/contact', {
+      /*.when('/contact', {
         templateUrl: 'views/contact.html',
         controller: 'ContactCtrl'
-      })
-      .when('/login', {
+      })*/
+      /*.when('/login', {
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl'
-      })
+      })*/
       .otherwise({
         redirectTo: '/'
       });
